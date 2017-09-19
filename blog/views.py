bf330479b78dc4ba06b3c2371863cebcdcff563d
Fernando Rodriguez <fernandomazariegos10@hotmail.es>
2017-09-19 10:53:21 -0600

    Agregar vistas para ver cada publicacion

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Publicar
+from django.shortcuts import render, get_object_or_404
 
 def listar_pub(request):
     posts = Publicar.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/listar_pub.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Publicar, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
